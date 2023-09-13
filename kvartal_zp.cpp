7c03008ae8137c3989934fb8eda18152aead9835
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-13 13:19:46 +0300

    Add files via upload

---
+++ kvartal_zp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    float zp1, zp2, zp3;
+    cout << "Зарплата за 1-й месяц = ";
+    cin >> zp1;
+    cout << "Зарплата за 2-й месяц = ";
+    cin >> zp2;
+    cout << "Зарплата за 3-й месяц = ";
+    cin >> zp3;
+    cout << "Зарплата за квартал " << zp1+zp2+zp3;
+
+}
