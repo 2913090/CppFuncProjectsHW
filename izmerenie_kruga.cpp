37076688dba05684da2bb28a045cd477d2ebb8d2
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-13 13:02:50 +0300

    Add files via upload

---
+++ izmerenie_kruga.cpp
@@ -0,0 +1,15 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    float r;
+    cout <<"Радиус круга = ";
+    cin >> r;
+    cout << "Площадь круга = " << r * r * 3.14 << "\n";
+    cout << "Длина окружности = " << r * 6.28 << "\n";
+    cout << "Диаметр круга = " <<  r * 2 << "\n";
+}
