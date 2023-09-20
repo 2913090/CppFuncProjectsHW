6f110fa0ad4564a2c6930d0505923535cdaa9f78
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-20 12:25:17 +0300

    Add files via upload

---
+++ max_chislo.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int chisla[7],q;
+    using namespace std;
+    setlocale(LC_ALL, "");
+    cout << "Введите семь целых чисел\n";
+    cin >> chisla[0] >> chisla[1] >> chisla[2] >> chisla[3] >> chisla[4] >> chisla[5] >> chisla[6];
+    for (int w{ 1 }; w < 7; w++) {
+        q = chisla[0];
+        if (chisla[w] > q) {
+            q = chisla[w];
+        }
+    }
+    cout << "наибольшее число из заданных - " << q;
+    }
