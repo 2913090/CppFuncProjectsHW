01c45ce1da49e0ec3695a2e3bf2edbe6ede7ac63
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-20 10:54:36 +0300

    Add files via upload

---
+++ chitirehznachniy_razvorot.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    int chislo;
+    using namespace std;
+    setlocale(LC_ALL, "");
+    cout << "Введите четырехзначное число\n";
+    cin >> chislo;
+    if (chislo / 1000 == 0 || chislo / 10000 > 0) {
+        cout << "Введенные данные не корректны";
+    }
+    else {
+        cout << (chislo / 1000) * 100 + ((chislo / 100) % 10) * 1000 + ((chislo / 10) % 10) + (chislo % 10) * 10;
+    }
+    }
