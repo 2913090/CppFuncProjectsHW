18d7e13e470e63ab72faf5ea12e0f89ea37e1250
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-27 14:15:47 +0300

    Add files via upload

---
+++ mobilnie_operatori.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float vremya;
+    short oper;
+    setlocale(LC_ALL, "");
+    cout << "Сколько минут длился ваш разговор?\n";
+    cin >> vremya;
+    if (vremya >= 0) {
+        cout << "Ваш мобильный оператор:(1,2,3)\n";
+        cin >> oper;
+        switch (oper) {
+        case 1:
+            cout << "Ваш разговор стоит " << vremya * 2 << " рублей";
+            break;
+        case 2:
+            cout << "Ваш разговор стоит " << vremya * 1.6 << " рублей";
+            break;
+        case 3:
+            cout << "Ваш разговор стоит " << vremya * 2.3 << " рублей";
+            break;
+        default:
+            cout << "Введенные данные некорректны";
+        }
+    }
+    else
+        cout << "Введенные данные некорректны";
+}
