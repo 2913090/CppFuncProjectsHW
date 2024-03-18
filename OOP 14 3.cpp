b3cc0c45f6553357b416b406102750d1ef8009f7
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-03-18 14:47:51 +0300

    OOP 14 3.cpp

--- OOP 14 3.cpp
+++ OOP 14 3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #define random(min, max) (min+rand()%(max-min+1))
 using namespace std;
@@ -6,35 +6,6 @@ using namespace std;
 const char LETTERS[]{ 'A','B', 'E','K','M','H','O','P','C','T','X' };
 const string COLORS[]{ "white","black","yellow","blue","red" };
 const string MODELS[]{ "Mercedes", "Toyota","BMW","Lexus","Tesla" };
-//class Student {
-//private:
-//	string name;
-//	int age;
-//public:
-//	Student(string name, int age):Student(name) {
-//		this->name = name;
-//		this->age = age;
-//	}
-//	Student(string name) {
-//		this->name = name;
-//		this->age = 0;
-//	}
-//	string getName() {
-//		return name;
-//	}
-//	int getAge() {
-//		return age;
-//	}
-//	void setName(string name) {
-//		this->name = name;
-//	}
-//	void setAge(int age) {
-//		this->age = age;
-//	}
-//	void showInfo() {
-//		cout << "Name: " << this->name << "\nAge: " << this->age << endl;
-//	}
-//};
 
 class Car {
 private:
@@ -77,15 +48,10 @@ private:
 		letters = lastNumber[0];
 		letters += lastNumber[4];
 		letters += lastNumber[5];
-		cout << num << endl;
 		num += (lastNumber[1] - '0') * 100;
-		cout << num << endl;
 		num += (lastNumber[2] - '0') * 10;
-		cout << num << endl;
 		num += (lastNumber[3] - '0');
-		cout << num << endl;
 		num++;
-		cout << num << endl;
 		if (num == 1000) {
 			num = 0;
 			if (letters[2] == LETTERS[10]) {
@@ -221,31 +187,62 @@ public:
 	int getSize() {
 		return size;
 	}
+	void addCar(Car car) {
+		if (size >= capacity) {
+			cout << "No places left\n";
+			return;
+		}
+		for (int i = 0; i < size; i++) {
+			if (car.getNumber() == cars[i].getNumber()) {
+				cout << "That car is already here\n";
+				return;
+			}
+		}
+		Car* buf = new Car[size + 1];
+		for (int i = 0; i < size; i++) {
+			buf[i] = cars[i];
+		}
+		buf[size] = car;
+		delete[] cars;
+		cars = buf;
+		size++;
+	}
+
+
+	void removeCar(string number) {
+		bool flag = 1;
+		for (int i = 0; i < size;) {
+			if (number == cars[i].getNumber()) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag) {
+			cout << "That car is not here\n";
+			return;
+		}
+		Car* buf = new Car[size - 1];
+		for (int j = 0, i = 0; i < size; i++) {
+			if (number != cars[i].getNumber()) {
+				buf[j++] = cars[i];
+			}
+			
+		}
+		delete[] cars;
+		cars = buf;
+	}
+
+	void parkInfo() {
+		cout << "Parking name: " << name << endl;
+		cout << "Capacity: " << size << "/" << capacity << endl;
+		cout << "Price per hour: " << price << endl;
+		for (int i = 0; i < size; i++) {
+			cars[i].carInfo();
+			cout << endl;
+		}
+	}
 private:
-	//void addCar(Car car) {
-	//	if (size >= capacity) {
-	//		cout << "No places left\n";
-	//		return;
-	//	}
-	//	bool flag = 0;
-	//	for (int i = 0; i < size;) {
-	//		if (car.getNumber == cars[i].getNumber) {
-	//			flag = 1;
-	//			break;
-	//		}
-	//	}
-	//	if (flag) {
-	//		cout << "That car is already here\n";
-	//		return;
-	//	}
-	//	Car* buf = new Car[size + 1];
-	//	for (int i = 0; i < size;) {
-	//		buf[i] = cars[i];
-	//	}
-	//	buf[size] = car;
-	//	delete[] cars;
-	//	cars = buf;
-	//}
+
 };
 
 
@@ -259,19 +256,15 @@ string Car::lastNumber = "A000AA";
 int main()
 {
 	srand(time(NULL));
-	//Student st1("Oleg", 20);
-	//st1.showInfo();
-	Car car1("mashina", "white");
+	Car cars[10];
+	for (int i = 0; i < 10; i++) {
+		cars[i] = Car(MODELS[random(0, 4)], COLORS[random(0, 4)]);
+	}
+	Parking park1("Parkovka", 10, 1.5);
+	for (int i = 0; i < 10; i++) {
+		park1.addCar(cars[i]);
+	}
+	park1.addCar(cars[0]);
+	park1.parkInfo();
 
-	//Car cars[100];
-	//for (int i = 0; i < 100; i++) {
-	//	cars[i] = Car(MODELS[random(0, 4)], COLORS[random(0, 4)]);
-	//}
-	//for (int i = 0; i < 100; i++) {
-	//	cars[i].carInfo();
-	//}
-	//cars[99].setColor("grey");
-	//cars[99].carInfo();
-	Parking park1("qwerty", 10, 1.5);
-	cout << park1.getName();
 }
