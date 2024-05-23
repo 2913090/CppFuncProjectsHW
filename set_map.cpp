1a72e171706ad439f34aa5948c1fc432f4f5e01d
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-05-23 13:09:01 +0300

    Update set_map.cpp

--- set_map.cpp
+++ set_map.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T, typename T2>
-class MySet {
+class MyMap {
 private:
 	class Node {
 	public:
@@ -14,8 +14,8 @@ private:
 	};
 	Node* root;
 public:
-	MySet() : root(nullptr) {};
-	//~Set() {
+	MyMap() : root(nullptr) {};
+	//~MyMap() {
 
 	//}
 	void insert(T key, T2 value) {
@@ -29,6 +29,10 @@ public:
 	}
 	void remove(T key) {
 		removeRecursive(key, this);
+	}
+	T2& operator [](T key) {
+		if (findNodeRecursive(this->root, key) == nullptr) throw out_of_range("error");
+		return (findNodeRecursive(this->root, key)->value);
 	}
 private:
 	void showRecursive(Node* node) {
@@ -50,38 +54,69 @@ private:
 		}
 		return node;
 	}
-	void removeRecursive(T key, Node node) {
-		if (key > node.key) {
-			if (key == node.right.key) {
-				
-			}
-			else {
-				removeRecursive(key, node.right);
-			}
-			
+	void removeRecursive(T key, Node delNode) {
+		if (key > delNode->key) {
+			Node* preNode = new Node[findPreNodeRecursive(delNode, delNode->right, key)];
 		}
-		else if (key < node.key) {
-			removeRecursive(key, node.left);
-		}
-		if (node.left == nullptr && node.right == nullptr) {
-
-		}
-		else if ((node.left == nullptr && node.right != nullptr) || (node.left != nullptr && node.right == nullptr)) {
-
+		else if (key < delNode->key) {
+			Node* preNode = new Node[findPreNodeRecursive(delNode, delNode->left, key)];
 		}
 		else {
 
+			remove;
+		}
+		if (delNode->left == nullptr && delNode->right == nullptr) {
+			if (preNode->right == delNode) {
+				delete delNode;
+				preNode->right = nullptr;
+			}
+			else if (preNode->left == delNode) {
+				delete delNode;
+				preNode->left = nullptr;
+			}
+		}
+		else if ((delNode->left == nullptr && delNode->right != nullptr) || (delNode->left != nullptr && delNode->right == nullptr)) {
+
+		}
+		else {
+			if (delNode)
 		}
 	}
-	void 
+	 
+	Node* findPreNodeRecursive(Node* node1, Node* node2, T& key) {
+		if (node2 == nullptr) return nullptr;
+		else if (node2->key < key) {
+			node1 = node2;
+			node2 = node2->right;
+			return findPreNodeRecursive(node1, node2, key);
+		}
+		else if (node2->key > key) {
+			node1 = node2;
+			node2 = node2->left;
+			return findPreNodeRecursive(node1, node2, key);
+		}
+		else if (node2->key == key) return node1;
+	}
+
+	Node* findNodeRecursive(Node* node, T& key) {
+		if (key > node->key) return findNodeRecursive(node->right, key);
+		else if (key < node->key) return findNodeRecursive(node->left, key);
+		else if (key == node->key) return node;
+		else return nullptr;
+	}
+
+	Node* searchMinNode(Node* node) {
+
+	}
 };
 int main()
 {
-	MySet<int, char> set;
+	MyMap<int, char> set;
 	set.insert(1, 'q');
 	set.insert(2, 'w');
 	set.insert(3, 'e');
 	set.insert(4, 'r');
 	set.show();
+	cout << set[4];
 
 }
