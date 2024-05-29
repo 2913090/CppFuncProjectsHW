ee825dc92dd85920fdd1bdea2175bb7cf66c36ed
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-05-29 14:41:19 +0300

    Add files via upload

---
+++ shooter.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Human {
+
+};
+class Weapon : public Human{
+
+};
+
+class AK74 : public Weapon {
+
+};
+
+class M416 : public Weapon {
+
+};
+
+class M9Barrete : public Weapon {
+
+};
+
+class Ammo : public Weapon {
+protected:
+	int damage;
+public:
+	virtual int getDamage() = 0;
+};
+
+class Ammo556 : public Ammo {
+
+};
+
+class Ammo556PS : public Ammo556 {
+	Ammo556PS(int damage) {
+		this->damage = damage;
+	}
+};
+
+class Ammo556BP : public Ammo556{
+	Ammo556BP(int damage) {
+		this->damage = damage;
+	}
+};
+
+class Ammo762 : public Ammo {
+
+};
+
+class Ammo762PS : public Ammo762 {
+	Ammo762PS(int damage) {
+		this->damage = damage;
+	}
+};
+
+class Ammo762BP : public Ammo762 {
+	Ammo762BP(int damage) {
+		this->damage = damage;
+	}
+};
+
+class Ammo919 : public Ammo {
+
+};
+
+class Ammo919PS : public Ammo919 {
+	Ammo919PS(int damage) {
+		this->damage = damage;
+	}
+};
+
+class Ammo919BP : public Ammo919 {
+	Ammo919BP(int damage) {
+		this->damage = damage;
+	}
+};
+
+int main()
+{
+
+}
