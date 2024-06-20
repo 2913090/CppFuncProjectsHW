308763b944fb15ad23abb4139926ce22129b88d9
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-06-20 14:51:44 +0300

    Add files via upload

---
+++ fstream.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream out;
+    out.open("text.txt");
+    if (out.is_open()) {
+        out << "Hello world" << endl;
+    }
+    out.close();
+
+
+
+
+
+
+
+
+    ifstream in("text.txt");
+    string line = "";
+    if (in.is_open()) {
+        while (getline(in,line)) {
+            cout << line << endl;
+        }
+    }
+    in.close();
+
+}
